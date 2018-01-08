b9c97f8b98ebbe86c31751084175e1196502ee17
mac <gabrielsantosdsm@gmail.com>
2018-01-08 18:37:38 -0200

    Initial commit

---
+++ app/src/main/java/vivacity/com/br/googlespeech/MainActivity.java
@@ -0,0 +1,21 @@
+package vivacity.com.br.googlespeech;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    public void test(View view) {
+        Toast.makeText(MainActivity.this, "Testing", Toast.LENGTH_SHORT).show();
+    }
+}
